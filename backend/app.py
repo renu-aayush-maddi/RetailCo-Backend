6919cb0ecc27f45d974d055e8ef5e63564e5ec1b
renu-aayush-maddi <2200032294@kluniversity.in>
2025-09-30 19:23:18 +0530

    explicit mention

--- backend/app.py
+++ backend/app.py
@@ -346,13 +346,14 @@ app = FastAPI(
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://coruscating-faun-e8a4fc.netlify.app",
     # add your deployed frontend domain(s) here
     # "https://yourapp.netlify.app",
     
 ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://*", "https://*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
